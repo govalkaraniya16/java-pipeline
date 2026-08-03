4df552cc888286df62740ece12bb40d7b17b9064
govalkaraniya16 <govalkaraniya16@gmail.com>
2026-08-03 15:17:25 +0530

    Update and rename hello.java to Hello.java

---
+++ Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+  public Static void main (string[] args)
+  {
+    Systen.out.print("hello, World!");
+  }
+}  
+  
--- hello.java
+++
@@ -1,7 +0,0 @@
-public class hello {
-  public Static void main (string[] ards)
-  {
-    Systen.out.print("hello, World!");
-  }
-}  
-  
